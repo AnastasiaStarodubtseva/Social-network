622eba80502345f6b43270ed0466dc07eb66e1d7
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2021-03-01 16:39:51 +0200

    Splitting components into subcomponents

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import './Navbar.css';
+import s from './Navbar.module.css';
+
+// let s = {
+//   'nav' : 'Navbar_nav__2imOE',
+//   'item' : 'Navbar_item__zmMk4',
+//   'active' : ''
+// }
+
+let c1 = 'item';
+let c2 = 'active';
+//'item active'
+let classes = c1 + '' + c2;
+let classesNew = `${s.item} ${s.active}`;
 
 const Navbar = () => {
   return (
-    <nav className='nav'>
-      <div className='item'>
+    <nav className={s.nav}>
+      <div className={s.item}>
         <a>Profile</a>
       </div>
-      <div className='item'>
+      <div className= {`${s.item} ${s.active}`}>
         <a>Messages</a>
       </div>
-      <div className='item'>
+      <div className={s.item}>
         <a>News</a>
       </div>
-      <div className='item'>
+      <div className={s.item}>
         <a>Music</a>
       </div>
-      <div className='item'>
+      <div className={s.item}>
         <a>Settings</a>
       </div>
     </nav>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import './Profile.css';
+import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={s.content}>
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtTkg5CG_CA9QjHyPlzZtGavaWebAl-ud_bA&usqp=CAU'/>
     <div>
       ava + discription
     </div>
-    <div>
-      My post
-    <div>New post</div>
-      <div>
-        <div>Post 1</div>
-        <div>Post 2</div>
-      </div>
+    <MyPosts />
     </div>
-  </div>
   )
 }
 
