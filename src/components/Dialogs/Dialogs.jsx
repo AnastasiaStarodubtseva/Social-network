c8b19aed5875a098bf0692dcf995cc06464af564
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2021-03-02 15:32:08 +0200

    Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Danila
+        </div>
+        <div className={s.dialog}>
+          Dima
+        </div>
+        <div className={s.dialog}>
+          Sveta
+        </div>
+        <div className={s.dialog}>
+          Maksym
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.dialog}>Hi</div>
+        <div className={s.dialog}>How are you?</div>
+        <div className={s.dialog}>Have a good day!</div>
+      </div>
     </div>
   )
 }
