a592a65abe09a20ca9fb92749e9b05b358c2971e
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2021-03-09 11:19:47 +0200

    Data transfer to state

---
+++ src/components/redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+  posts: [
+    {id: '1', message: 'Hi, how are you', likesCount: 12},
+    {id: '2', message: 'It is my first post', likesCount: 10}
+  ],
+  dialogs: [
+    {id: '1', name: 'Francis'},
+    {id: '2', name: 'Benjamin'},
+    {id: '3', name: 'Olivia'},
+    {id: '4', name: 'Kate'},
+    {id: '5', name: 'Sean'},
+    {id: '6', name: 'Clifford'}
+  ],
+  messages: [
+    {id: '1', message: 'Hi!'},
+    {id: '2', message: 'How are you?'},
+    {id: '3', message: 'What is your name?'},
+    {id: '4', message: 'How old are you?'},
+    {id: '5', message: 'Have a good day!'},
+    {id: '6', message: 'Good bye!'}
+  ]
+}
+
+export default state;
--- src/index.js
+++ src/index.js
@@ -3,33 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let posts = [
-  {id: '1', message: 'Hi, how are you', likesCount: 12},
-  {id: '2', message: 'It is my first post', likesCount: 10}
-]
-
-let dialogs = [
-  {id: '1', name: 'Francis'},
-  {id: '2', name: 'Benjamin'},
-  {id: '3', name: 'Olivia'},
-  {id: '4', name: 'Kate'},
-  {id: '5', name: 'Sean'},
-  {id: '6', name: 'Clifford'}
-]
-
-let messages = [
-  {id: '1', message: 'Hi!'},
-  {id: '2', message: 'How are you?'},
-  {id: '3', message: 'What is your name?'},
-  {id: '4', message: 'How old are you?'},
-  {id: '5', message: 'Have a good day!'},
-  {id: '6', message: 'Good bye!'}
-]
+import state from './components/redux/state'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App appState={state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
